05af217a298f1a1d8ef9fa77f32a3f8704d70dd9
Aniket Bhoir <63443038+Aniket-373@users.noreply.github.com>
2023-07-23 01:21:15 -0700

    Add files via upload

---
+++ precache-manifest.82bf40aa43f675525aacaa357d61204e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4411be4493ed15d939e",
+    "url": "/Texting/static/css/main.76200e07.chunk.css"
+  },
+  {
+    "revision": "d4411be4493ed15d939e",
+    "url": "/Texting/static/js/main.d4411be4.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/Texting/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "37ab2e15c473be5595cc",
+    "url": "/Texting/static/js/2.37ab2e15.chunk.js"
+  },
+  {
+    "revision": "caeb27ea528944cd8ffb",
+    "url": "/Texting/static/js/runtime~main.caeb27ea.js"
+  },
+  {
+    "revision": "de4acb4b5bf9fa4935f7a5523be4df0a",
+    "url": "/Texting/index.html"
+  }
+];
