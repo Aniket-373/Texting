8268310d56d390a427d6154831447116faabd818
Aniket Bhoir <63443038+Aniket-373@users.noreply.github.com>
2023-07-23 01:51:39 -0700

    Updates

---
+++ precache-manifest.a9bb075f0be6130a1e94e1d2b5a3d9e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fc3220a11087e69c0eb",
+    "url": "/Texting/static/css/main.223580f4.chunk.css"
+  },
+  {
+    "revision": "1fc3220a11087e69c0eb",
+    "url": "/Texting/static/js/main.1fc3220a.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/Texting/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "11a3eb75ba923fb4e350",
+    "url": "/Texting/static/js/2.11a3eb75.chunk.js"
+  },
+  {
+    "revision": "470f8f0d5e2da00e4e5c",
+    "url": "/Texting/static/js/runtime~main.470f8f0d.js"
+  },
+  {
+    "revision": "c2f30ad2f658e758f9b5c3b88eb7da89",
+    "url": "/Texting/index.html"
+  }
+];
